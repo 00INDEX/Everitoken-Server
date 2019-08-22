6c5757e8a7ea7e534b8ccc3853888580479f1e97
1269473699 <1269473699@qq.com>
2019-08-22 15:01:48 +0800

    Try to complete some unfinished methods

--- src/main/java/everitoken/dao/impl/CustomerRepositoryImpl.java
+++ src/main/java/everitoken/dao/impl/CustomerRepositoryImpl.java
@@ -19,7 +19,25 @@ public class CustomerRepositoryImpl implements CustomerRepository {
 
     @Override
     public CustomerEntity get(Integer id) {
-        return null;
+        CustomerEntity c = null;
+        cfg = new Configuration();
+        cfg.configure();
+        SessionFactory sessionFactory = cfg.buildSessionFactory();
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+
+        try {
+            c = session.get(CustomerEntity.class, id);
+        }catch (Exception e){
+            e.printStackTrace();
+            Exception exception = new Exception("数据库异常");
+            //throw exception;
+        }finally {
+            transaction.commit();
+            session.close();
+            sessionFactory.close();
+        }
+        return c;
     }
 
     @Override
@@ -56,12 +74,45 @@ public class CustomerRepositoryImpl implements CustomerRepository {
 
     @Override
     public void update(CustomerEntity entity) {
+        cfg = new Configuration();
+        cfg.configure();
+        SessionFactory sessionFactory = cfg.buildSessionFactory();
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
 
+        try {
+            session.saveOrUpdate(entity);
+        }catch (Exception e){
+            e.printStackTrace();
+            Exception exception = new Exception("数据库异常");
+            //throw exception;
+        }finally {
+            transaction.commit();
+            session.close();
+            sessionFactory.close();
+        }
     }
 
     @Override
     public void delete(Integer id) {
+        cfg = new Configuration();
+        cfg.configure();
+        SessionFactory sessionFactory = cfg.buildSessionFactory();
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
 
+        try {
+            CustomerEntity c = session.get(CustomerEntity.class, id);
+            session.delete(c);
+        }catch (Exception e){
+            e.printStackTrace();
+            Exception exception = new Exception("数据库异常");
+            //throw exception;
+        }finally {
+            transaction.commit();
+            session.close();
+            sessionFactory.close();
+        }
     }
 
     @Override
