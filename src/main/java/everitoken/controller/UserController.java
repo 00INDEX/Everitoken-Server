26c6ac804bb95960dd3cb459495ca7fee928128d
Zechen6 <44701774+Zechen6@users.noreply.github.com>
2019-08-23 03:12:12 +0000

    Update UserController.java

--- src/main/java/everitoken/controller/UserController.java
+++ src/main/java/everitoken/controller/UserController.java
@@ -1,11 +1,9 @@
 package everitoken.controller;
 
-import everitoken.dao.impl.CustomerRepositoryImpl;
-import everitoken.dao.impl.ProducterRepositoryImpl;
-import everitoken.dao.impl.UserRepositoryImpl;
-import everitoken.entity.CustomerEntity;
-import everitoken.entity.ProducerEntity;
-import everitoken.entity.UserEntity;
+import everitoken.dao.GovernmentRepository;
+import everitoken.dao.RecyclingStationRepository;
+import everitoken.dao.impl.*;
+import everitoken.entity.*;
 import org.springframework.stereotype.Controller;
 
 import org.springframework.web.bind.annotation.RequestBody;
@@ -23,6 +21,8 @@ public class UserController {
     private CustomerRepositoryImpl customerRepository;
     private UserRepositoryImpl userRepository;
     private ProducterRepositoryImpl producterRepository;
+    private GovernmentRepositoryImpl governmentRepository;
+    private RecyclingStationRepositoryImpl recyclingStationRepository;
     private Map<String, Object> res;
     /**
      * 客户注册方法
@@ -55,7 +55,7 @@ public class UserController {
         if (data.containsKey("username")) userEntity.setUsername(data.get("username").toString());
         if (data.containsKey("password")) userEntity.setPassword(data.get("password").toString());
         if (data.containsKey("email")) userEntity.setEmail(data.get("email").toString());
-        if (data.containsKey("type")) userEntity.setType((Integer)data.get("email"));
+        if (data.containsKey("type")) userEntity.setType((Integer)data.get("type"));
         if (data.containsKey("sex")) customerEntity.setCustomerSex(Integer.parseInt(data.get("sex").toString()));
         if (data.containsKey("idnumber")) customerEntity.setCustomerIdnumber(data.get("idnumber").toString());
         if (data.containsKey("phone")) customerEntity.setCustomerPhone(data.get("phone").toString());
@@ -80,8 +80,8 @@ public class UserController {
      * @param httpSession session数据
      * @param data 用户数据
      * @return
-
-    @RequestMapping(value = "/customer/register", method = RequestMethod.POST)
+     */
+    @RequestMapping(value = "/producer/register", method = RequestMethod.POST)
     @ResponseBody
     public Object producer_register(HttpSession httpSession, @RequestBody Map<String, Object> data){
         res = new HashMap<>();
@@ -102,17 +102,25 @@ public class UserController {
             res.put("msg", "缺失必要字段");
             return res;
         }
-        if (data.containsKey("name")) customerEntity.setCustomerName(data.get("name").toString());
+        if (data.containsKey("producer_name")) producerEntity.setProducerName(data.get("producer_name").toString());
         if (data.containsKey("username")) userEntity.setUsername(data.get("username").toString());
         if (data.containsKey("password")) userEntity.setPassword(data.get("password").toString());
         if (data.containsKey("email")) userEntity.setEmail(data.get("email").toString());
-        if (data.containsKey("type")) userEntity.setType((Integer)data.get("email"));
-        if (data.containsKey("sex")) customerEntity.setCustomerSex(Integer.parseInt(data.get("sex").toString()));
-        if (data.containsKey("idnumber")) customerEntity.setCustomerIdnumber(data.get("idnumber").toString());
-        if (data.containsKey("phone")) customerEntity.setCustomerPhone(data.get("phone").toString());
+        if (data.containsKey("type")) userEntity.setType(Integer.parseInt(data.get("type").toString()));
+        if (data.containsKey("producer_private_key")) producerEntity.setProducerPrivateKey(data.get("producer_private_key").toString());
+        if (data.containsKey("authorized"))
+        {
+            Integer value=Integer.parseInt(data.get("authorized").toString());
+            producerEntity.setAuthorized(value.byteValue());
+        }
+        if(data.containsKey("producer_CHNCode")) producerEntity.setProducerChnCode(data.get("producer_CHNCode").toString());
+        if(data.containsKey("producer_uid")) producerEntity.setProducerUid(Integer.parseInt(data.get("producer_uid").toString()));
+        producerEntity.setProducerEmail(userEntity.getEmail());
+
+
         int uid = -1;
         try {
-            uid = customerRepository.add(customerEntity);
+            uid = producterRepository.add(producerEntity);
             userEntity.setInfoId(uid);
             uid = userRepository.add(userEntity);
         }catch (Exception e){
@@ -125,7 +133,117 @@ public class UserController {
         res.put("msg", "success");
         return res;
     }
+
+    /**
+     * 政府注册方法
+     * @param httpSession session数据
+     * @param data 政府数据
+     * @return
      */
+
+    @RequestMapping(value = "/government/register", method = RequestMethod.POST)
+    @ResponseBody
+    public Object government_register(HttpSession httpSession, @RequestBody Map<String, Object> data){
+        res = new HashMap<>();
+        GovernmentEntity governmentEntity = new GovernmentEntity();
+        UserEntity userEntity = new UserEntity();
+        governmentRepository = new GovernmentRepositoryImpl();
+        userRepository = new UserRepositoryImpl();
+        if (data.containsKey("username")){
+            UserEntity oldUser = userRepository.getByUsername((String)data.get("username"));
+            if (oldUser != null) {
+                res.put("code", 20006);
+                res.put("msg", "用户名重复");
+                return res;
+            }
+        }
+        if (!data.containsKey("username") || !data.containsKey("name") || !data.containsKey("password") || !data.containsKey("email") || !data.containsKey("sex") || !data.containsKey("idnumber") || !data.containsKey("phone")){
+            res.put("code", 20001);
+            res.put("msg", "缺失必要字段");
+            return res;
+        }
+        if (data.containsKey("government_name")) governmentEntity.setGovernmentName(data.get("government_name").toString());
+        if (data.containsKey("username")) userEntity.setUsername(data.get("username").toString());
+        if (data.containsKey("password")) userEntity.setPassword(data.get("password").toString());
+        if (data.containsKey("email")) userEntity.setEmail(data.get("email").toString());
+        if (data.containsKey("type")) userEntity.setType((Integer)data.get("type"));
+        if(data.containsKey("uid")) userEntity.setUid(Integer.parseInt(data.get("uid").toString()));
+        if (data.containsKey("government_private_key")) governmentEntity.setGovernmentPrivateKey(data.get("government_private_key").toString());
+        if(data.containsKey("government_CHNCode")) governmentEntity.setGovernmentChnCode(data.get("government_CHNCode").toString());
+        if(data.containsKey("government_uid")) governmentEntity.setGovernmentUid(Integer.parseInt(data.get("government_uid").toString()));
+
+
+
+        int uid = -1;
+        try {
+            uid = governmentRepository.add(governmentEntity);
+            userEntity.setInfoId(uid);
+            uid = userRepository.add(userEntity);
+        }catch (Exception e){
+            res.put("code", 20002);
+            res.put("msg", e.getMessage());
+            return res;
+        }
+        httpSession.setAttribute("uid", uid);
+        res.put("code", 0);
+        res.put("msg", "success");
+        return res;
+    }
+    /**
+     * 回收站注册方法
+     * @param httpSession session数据
+     * @param data 回收站数据
+     * @return
+     */
+
+    @RequestMapping(value = "/recycling_station/register", method = RequestMethod.POST)
+    @ResponseBody
+    public Object recycling_station_register(HttpSession httpSession, @RequestBody Map<String, Object> data){
+        res = new HashMap<>();
+        RecylingStationEntity recylingStationEntity = new RecylingStationEntity();
+        UserEntity userEntity = new UserEntity();
+        recyclingStationRepository=new RecyclingStationRepositoryImpl();
+        userRepository = new UserRepositoryImpl();
+        if (data.containsKey("username")){
+            UserEntity oldUser = userRepository.getByUsername((String)data.get("username"));
+            if (oldUser != null) {
+                res.put("code", 20006);
+                res.put("msg", "用户名重复");
+                return res;
+            }
+        }
+        if (!data.containsKey("username") || !data.containsKey("name") || !data.containsKey("password") || !data.containsKey("email") || !data.containsKey("sex") || !data.containsKey("idnumber") || !data.containsKey("phone")){
+            res.put("code", 20001);
+            res.put("msg", "缺失必要字段");
+            return res;
+        }
+        if (data.containsKey("rs_name")) recylingStationEntity.setRsName(data.get("rs_name").toString());
+        if (data.containsKey("username")) userEntity.setUsername(data.get("username").toString());
+        if (data.containsKey("password")) userEntity.setPassword(data.get("password").toString());
+        if (data.containsKey("email")) userEntity.setEmail(data.get("email").toString());
+        if (data.containsKey("type")) userEntity.setType((Integer)data.get("type"));
+        if(data.containsKey("uid")) userEntity.setUid(Integer.parseInt(data.get("uid").toString()));
+        if (data.containsKey("rs_private_key")) recylingStationEntity.setRsPrivateKey(data.get("rs_private_key").toString());
+        if(data.containsKey("rs_uid")) recylingStationEntity.setRsUid(Integer.parseInt(data.get("rs_uid").toString()));
+
+
+
+        int uid = -1;
+        try {
+            uid =recyclingStationRepository.add(recylingStationEntity);
+            userEntity.setInfoId(uid);
+            uid = userRepository.add(userEntity);
+        }catch (Exception e){
+            res.put("code", 20002);
+            res.put("msg", e.getMessage());
+            return res;
+        }
+        httpSession.setAttribute("uid", uid);
+        res.put("code", 0);
+        res.put("msg", "success");
+        return res;
+    }
+
     /**
      *
      * 用户登陆事件
