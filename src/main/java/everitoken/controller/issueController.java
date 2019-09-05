72afcf4403b0e70f7f6264e4370ca2f75099a1ae
Zechen6 <44701774+Zechen6@users.noreply.github.com>
2019-09-05 19:05:22 +0800

    Update issueController.java

--- src/main/java/everitoken/controller/issueController.java
+++ src/main/java/everitoken/controller/issueController.java
@@ -1,6 +1,6 @@
 package everitoken.controller;
 
-import EveritokenSDK.Action;
+import everitoken.EveriTokenOperation.Action;
 import everitoken.Operations.Operate;
 import everitoken.dao.BatteryRepository;
 import everitoken.dao.impl.BatteryRepositoryImpl;
@@ -18,9 +18,9 @@ import static everitoken.Operations.Operate.GetProducer;
 
 @RequestMapping(value = "/Manage")
 public class issueController {
-    @RequestMapping(value = "/IssueBattery",method = RequestMethod.POST)
+    @RequestMapping(value = "/issueBattery",method = RequestMethod.POST)
     @ResponseBody
-    public Object IssueBattery(@RequestBody Map<String,Object>data){//function:获取授权记录 need:生产商ID、电池名称、电池类型,电池循环充放电次数
+    public Object IssueBattery(@RequestBody Map<String,Object>data){//function:发行电池 need:生产商ID、电池名称、电池类型,电池循环充放电次数
         Map<String,Object> res=new HashMap<>();
         String BatteryName;
         Action action = new Action();
@@ -34,12 +34,27 @@ public class issueController {
             res.put("msg","缺少电池名称");
             return res;
         }
-        BatteryName = data.get("Battery_Name").toString();
+        if(!data.containsKey("type")
+                ||!data.containsKey("batteryMaxVoltage")
+                ||!data.containsKey("batteryAverageTemperature")
+                ||!data.containsKey("batteryChgCycles"))
+        {
+            res.put("code",10001);
+            res.put("msg","缺少电池信息");
+            return res;
+        }
+
+        BatteryName = data.get("BatteryName").toString();
         Integer ID = Integer.parseInt(data.get("id").toString());
         ProducerEntity producerEntity = GetProducer(ID);
         if(producerEntity==null){
             res.put("code",10005);
             res.put("msg","id不存在数据库");
+            return res;
+        }
+        if (producerEntity.getProducerAuthorized().equals("false")){
+            res.put("code",10006);
+            res.put("msg","该用户未被授权");
             return res;
         }
         if(!action.issueBattery(action.toPublicKey(producerEntity.getProducerPrivateKey()),
@@ -51,7 +66,7 @@ public class issueController {
         BatteryEntity batteryEntity = new BatteryEntity();
         BatteryRepository batteryRepository = new BatteryRepositoryImpl();
         if(data.containsKey("type")) batteryEntity.setBatteryType(Integer.parseInt(data.get("type").toString()));
-        if(data.containsKey("name")) batteryEntity.setUid(Integer.parseInt(data.get("name").toString()));
+        if(data.containsKey("BatteryName")) batteryEntity.setUid(Integer.parseInt(data.get("BatteryName").toString()));
         if(data.containsKey("batteryMaxVoltage")) batteryEntity.setBatteryMaxVoltage(data.get("batteryMaxVoltage").toString());
         if(data.containsKey("batteryAverageTemperature")) batteryEntity.setBatteryAverageTemperature(data.get("batteryAverageTemperature").toString());
         if(data.containsKey("batteryChgCycles")) batteryEntity.setBatteryChgCycles(Integer.parseInt(data.get("batteryChgCycles").toString()));
@@ -80,5 +95,12 @@ public class issueController {
         res.put("code",0);
         res.put("msg","success");
         return res;
+    }
+    @RequestMapping(value = "/Test",method = RequestMethod.POST)
+    @ResponseBody
+    public Object Test(@RequestBody Map<String,Object>data){
+        data.put("id",55555);
+
+        return data;
     }
 }
